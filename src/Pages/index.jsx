36aafd473971d0026405527e12576c13cd1c1041
umerKhan4ek <umerKhan4ek@gmail.com>
2022-01-09 12:09:57 +0500

    country-added

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
-import { Form, Radio, Input, Space, Button } from "antd";
+import { Form, Radio, Input, Space,Tooltip, Button } from "antd";
 import { Allquestions } from "../contstants/question";
 
 import { Menu, Dropdown } from "antd";
 import { DownOutlined } from "@ant-design/icons";
+
+import CountryDropdown from 'country-dropdown-with-flags-for-react';
+
+import { Us } from 'react-flags-select';
+
+import ReactFlagsSelect from 'react-flags-select';
 
 function SurveyForm() {
   const [questions, setQuestions] = useState(Allquestions);
@@ -13,14 +19,17 @@ function SurveyForm() {
 
   const [showData, setShowData] = useState(false);
 
+  const [selected, setSelected] = useState('');
+
   const showQuestionData = () => {
-    setShowData(true)
-  }
+    setShowData(!showData);
+  };
 
   const menu = (
     <Menu onClick={showQuestionData}>
-      <Menu.Item key="0">Show QUestion</Menu.Item>
-      <Menu.Item key="1"></Menu.Item>
+      <Menu.Item key="0">  <div>
+        <Us /> United States
+      </div></Menu.Item>
     </Menu>
   );
 
@@ -40,73 +49,59 @@ function SurveyForm() {
     setCurrentQuestion(currentQuestion + 1);
   };
 
+  const handleSelection = (e) =>
+  {
+    setShowData(!showData);
+    setSelected(e)
+  }
+
   return (
     <div className="survey-wrapper">
       <div className="cards-section">
-        <h2 className="title">Survey Form</h2>
+        <h2 className="title">Prefrence Wizard</h2>
 
         <div className="card-form">
           {currentQuestion < questions.length ? (
-            <Form name="basic" onFinish={onFinish}>
-              <Dropdown overlay={menu} trigger={["click"]}>
-                <a
-                  className="ant-dropdown-link"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Show Question <DownOutlined />
-                </a>
-              </Dropdown>
+            <Form name="basic" onFinish={onFinish}  layout="vertical">
+              {/* <Dropdown overlay={menu} trigger={["click"]}>
+                <Button size="large">
+                  bottomLeft <DownOutlined />
+                </Button>
+              </Dropdown> */}
+
+
+              <ReactFlagsSelect
+                countries={["US", "CA","MX"]}
+        selected={selected}
+        onSelect={handleSelection}
+      />
 
               {showData && (
                 <>
                   <div className="question">
                     <h2 className="label">Question: {currentQuestion + 1}</h2>
+
                     <h3 className="text">
                       {questions[currentQuestion]?.question}
                     </h3>
+                    <Form.Item label="Answer"  tooltip="Input A Number">
+                     
+                        <Input
+                          placeholder="Input a number"
+                          size="large"
+                          type='number'
+                        />
+                      
+                    </Form.Item>
                   </div>
-                  <Form.Item name="radio">
-                    <Radio.Group onChange={onChange} value={radioOption}>
-                      <Space direction="vertical">
-                        {questions[currentQuestion]?.option1 && (
-                          <Radio
-                            className="radio-option"
-                            value={questions[currentQuestion].option1}
-                          >
-                            {questions[currentQuestion].option1}
-                          </Radio>
-                        )}
-                        {questions[currentQuestion]?.option2 && (
-                          <Radio
-                            className="radio-option"
-                            value={questions[currentQuestion].option2}
-                          >
-                            {questions[currentQuestion].option2}
-                          </Radio>
-                        )}
-                        {questions[currentQuestion]?.option3 && (
-                          <Radio
-                            className="radio-option"
-                            value={questions[currentQuestion].option3}
-                          >
-                            {questions[currentQuestion].option3}
-                          </Radio>
-                        )}
-                        {questions[currentQuestion]?.option4 && (
-                          <Radio
-                            className="radio-option"
-                            value={questions[currentQuestion].option4}
-                          >
-                            {questions[currentQuestion].option4}
-                          </Radio>
-                        )}
-                      </Space>
-                    </Radio.Group>
-                  </Form.Item>
                 </>
               )}
               <div className="form-buttons">
-                <Button type="primary" onClick={nextQuestion}>
+                <Button
+                  type="primary"
+                  className="nextBtn"
+                  onClick={nextQuestion}
+                >
                   Next
                 </Button>
               </div>
@@ -121,3 +116,48 @@ function SurveyForm() {
 }
 
 export default SurveyForm;
+
+// <div className="question">
+// <h2 className="label">Question: {currentQuestion + 1}</h2>
+// <h3 className="text">
+//   {questions[currentQuestion]?.question}
+// </h3>
+// </div>
+// <Form.Item name="radio">
+// <Radio.Group onChange={onChange} value={radioOption}>
+//   <Space direction="vertical">
+//     {questions[currentQuestion]?.option1 && (
+//       <Radio
+//         className="radio-option"
+//         value={questions[currentQuestion].option1}
+//       >
+//         {questions[currentQuestion].option1}
+//       </Radio>
+//     )}
+//     {questions[currentQuestion]?.option2 && (
+//       <Radio
+//         className="radio-option"
+//         value={questions[currentQuestion].option2}
+//       >
+//         {questions[currentQuestion].option2}
+//       </Radio>
+//     )}
+//     {questions[currentQuestion]?.option3 && (
+//       <Radio
+//         className="radio-option"
+//         value={questions[currentQuestion].option3}
+//       >
+//         {questions[currentQuestion].option3}
+//       </Radio>
+//     )}
+//     {questions[currentQuestion]?.option4 && (
+//       <Radio
+//         className="radio-option"
+//         value={questions[currentQuestion].option4}
+//       >
+//         {questions[currentQuestion].option4}
+//       </Radio>
+//     )}
+//   </Space>
+// </Radio.Group>
+// </Form.Item>
